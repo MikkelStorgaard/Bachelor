c5191e327773a988117a0a3aa4c1d19cd15fbabe
Mikkel Storgaard Knudsen <mikkel.storgaard@eksponent.com>
2016-06-06 19:28:43 +0200

    pøls

--- mine_tekster/kapitler/auto/structures.el
+++ mine_tekster/kapitler/auto/structures.el
@@ -4,7 +4,20 @@
    (LaTeX-add-labels
     "sec:structures"
     "subsec:structuresaregood"
-    "structure_rule"
-    "interpretingfunctionsandtypeswithstructures"))
+    "Rule1"
+    "normaldecls"
+    "Rule2type"
+    "Rule2fun"
+    "Rule2sig"
+    "Rule4"
+    "Rule3"
+    "Rule4a"
+    "interpretingfunctionsandtypeswithstructures"
+    "Rule5"
+    "subsec:checkingforduplicates"
+    "chunks"
+    "subsec:checkfunortype"
+    "resolvelongname"
+    "subsec:structuretests"))
  :latex)
 
