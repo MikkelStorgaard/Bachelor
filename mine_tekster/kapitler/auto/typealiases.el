b4d85290859708257c45adeaf1a3af444c7f39c6
Mikkel Storgaard Knudsen <mikkel.storgaard@eksponent.com>
2016-06-11 17:17:15 +0200

    HEJ!

--- mine_tekster/kapitler/auto/typealiases.el
+++ mine_tekster/kapitler/auto/typealiases.el
@@ -2,11 +2,13 @@
  "typealiases"
  (lambda ()
    (LaTeX-add-labels
+    "futharks_types"
     "typealiasinterference"
     "typealiasimplementation"
     "longchain"
     "cyclicaldefinitionerror"
     "typealiasingcode"
-    "typeclarification"))
+    "typeclarification"
+    "nbody"))
  :latex)
 
