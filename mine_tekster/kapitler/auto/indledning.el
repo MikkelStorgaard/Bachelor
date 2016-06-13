a7e8932344f0c84330253ecec2c41c74f0a5c25d
Mikkel Storgaard Knudsen <mikkelstorgaard@gmail.com>
2016-06-13 10:29:52 +0200

    m

--- mine_tekster/kapitler/auto/indledning.el
+++ mine_tekster/kapitler/auto/indledning.el
@@ -4,7 +4,9 @@
    (LaTeX-add-labels
     "sec:introduction"
     "subsec:label"
+    "sphere_example"
     "subsec:higherorderfunctionality"
+    "struct_vec3"
     "first_functor_example"
     "subsec:probdef"
     "subsec:project_scope"))
